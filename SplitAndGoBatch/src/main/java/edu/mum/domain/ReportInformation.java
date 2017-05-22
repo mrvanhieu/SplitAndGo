c2123e21a3f029613d546dcc04561079e0ff241f
hungdinhnguyen <hungdinhnguyen@gmail.com>
2017-05-22 00:18:36 -0500

    Add new Batch source

---
+++ SplitAndGoBatch/src/main/java/edu/mum/domain/ReportInformation.java
@@ -0,0 +1,54 @@
+package edu.mum.domain;
+
+public class ReportInformation {
+
+	private long id;
+
+	private String firstName;
+
+	private String lastName;
+
+	private String nickName;
+
+	private String email;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
---
+++ SplitAndGoBatch/src/main/java/edu/mum/main/Main.java
@@ -0,0 +1,45 @@
+package edu.mum.main;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Main obj = new Main();
+		obj.run();
+
+	}
+
+	private void run() {
+
+		String[] springConfig = { "context/applicationContext.xml", "context/daily-job.xml" };
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(springConfig);
+
+		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+		Job job = (Job) context.getBean("dailyJob");
+
+		try {
+
+			JobParameters param = new JobParameters();
+			JobExecution execution = jobLauncher.run(job, param);
+			System.out.println("Exit Status : " + execution.getStatus());
+			System.out.println("Exit Status : " + execution.getAllFailureExceptions());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+
+		System.out.println("Done");
+
+	}
+
+}
