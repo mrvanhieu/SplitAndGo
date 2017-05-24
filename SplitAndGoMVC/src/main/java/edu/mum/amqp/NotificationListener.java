8fa0f4c2bd0d3a1cf03e02b11f27d1e23597452e
hungdinhnguyen <hungdinhnguyen@gmail.com>
2017-05-23 22:26:17 -0500

    Update Notification
    - Merge Producer and Consumer Context
    - Correct Listener

--- SplitAndGoMVC/src/main/java/edu/mum/amqp/NotificationListener.java
+++ SplitAndGoMVC/src/main/java/edu/mum/amqp/NotificationListener.java
@@ -1,17 +1,15 @@
 package edu.mum.amqp;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import edu.mum.domain.Notification;
 import edu.mum.service.NotificationService;
+import edu.mum.service.impl.NotificationServiceImpl;
 
 public class NotificationListener {
 
-	@Autowired
 	NotificationService notificationService;
 
 	public void listen(Notification notification) {
-
+		notificationService = new NotificationServiceImpl();
 		try {
 			notificationService.save(notification);
 		} catch (RuntimeException ex) {
