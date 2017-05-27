45c90c55c9097d8d4fcaefac7b9d16a963d3817e
Hieu Ho <mrvanhieu@gmail.com>
2017-05-27 16:09:59 -0500

    syc the populate.sql

--- SplitAndGoMVC/src/sql/populate.sql
+++ SplitAndGoMVC/src/sql/populate.sql
@@ -8,5 +8,17 @@ INSERT INTO authority (username, authority) VALUES ('admin', 'ROLE_USER');
 INSERT INTO `MEMBER` (firstname, lastname, nickname, email, gender, credential_id) VALUES ('Tuyen', 'Lam', 'Tien' ,'tuyenrg@gmail.com', 0 ,'admin');
 INSERT INTO `MEMBER` (firstname, lastname, nickname, email, gender, credential_id) VALUES ('Guest', 'Guest', 'Guest' ,'guest@gmail.com', 0, 'guest');
 
-INSERT INTO `TRIP` (name, description, startDate, endDate) VALUES ('Trip 1', 'Trip 1', '2017-05-22', '2017-05-30');
-INSERT INTO `TRIP` (name, description, startDate, endDate) VALUES ('Trip 2', 'Trip 2', '2017-06-22', '2017-06-28');
+INSERT INTO Trip (`id`, `description`, `duration`, `endDate`, `name`, `startDate`) VALUES ('1', 'First Trip', '30', '2017-10-10', 'First Trip', '2017-09-09');
+INSERT INTO Trip (`id`, `description`, `duration`, `endDate`, `name`, `startDate`) VALUES ('2', 'Second Trip', '30', '2017-07-07', 'Second Trip', '2017-08-08');
+INSERT INTO Fund (totalAmount, remainingAmount, trip_id) VALUES (200, 200, 1);
+INSERT INTO Fund (totalAmount, remainingAmount, trip_id) VALUES (300, 300, 2);
+
+INSERT INTO Payment (`id`, `amount`, `date`, `description`, trip_id) VALUES ('2', '100', '2017-10-10', 'Second payment', '1');
+INSERT INTO Payment (`id`, `amount`, `date`, `description`, trip_id) VALUES ('1', '200', '2017-10-10', 'First payment', '1')
+INSERT INTO Payment (`id`, `amount`, `date`, `description`, trip_id) VALUES ('3', '100', '2017-09-09', 'Third payment', '1');
+INSERT INTO Payment (`id`, `amount`, `date`, `description`, trip_id) VALUES ('4', '200', '2017-09-09', 'Fourth payment', '1')
+
+INSERT INTO Payment (`id`, `amount`, `date`, `description`, trip_id) VALUES ('5', '100', '2017-07-07', 'Second payment', '2');
+INSERT INTO Payment (`id`, `amount`, `date`, `description`, trip_id) VALUES ('6', '200', '2017-07-07', 'First payment', '2')
+INSERT INTO Payment (`id`, `amount`, `date`, `description`, trip_id) VALUES ('7', '25', '2017-08-08', 'Third payment', '2');
+INSERT INTO Payment (`id`, `amount`, `date`, `description`, trip_id) VALUES ('8', '30', '2017-08-08', 'Fourth payment', '2')
